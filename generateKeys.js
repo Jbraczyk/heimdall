3232f8a7f209dec088a1e241bcc6f77418481b30
jbraczyk <bouratchikj@gmail.com>
2022-07-07 05:53:15 +0200

    # eslint generateKeys

--- generateKeys.js
+++ generateKeys.js
@@ -1,26 +1,30 @@
-const { generateKeyPair } = require('crypto')
-const config = require('./config.json')
-const fs = require('fs')
+const { generateKeyPair } = require("crypto")
+const config = require("./config.json")
+const fs = require("fs")
 
 /** [generateKeyPair ~ Generate our key for bot and server communication] */
-generateKeyPair('rsa', {
+generateKeyPair(
+  "rsa",
+  {
     modulusLength: 4096,
     publicKeyEncoding: {
-        type: 'spki',
-        format: 'pem'
+      type: "spki",
+      format: "pem",
     },
     privateKeyEncoding: {
-        type: 'pkcs8',
-        format: 'pem',
-        cipher: 'aes-256-cbc',
-        passphrase: 'saucetomate'
-    }
-}, (err, publicKey, privateKey) => {
-    if(err){
-        console.log(err);
-        return false
+      type: "pkcs8",
+      format: "pem",
+      cipher: "aes-256-cbc",
+      passphrase: "saucetomate",
+    },
+  },
+  (err, publicKey, privateKey) => {
+    if (err) {
+      console.log(err)
+      return false
     }
 
-    fs.writeFileSync(config.keyFile, privateKey, 'utf-8')
-    console.log('KeyGenerated');
-});
+    fs.writeFileSync(config.keyFile, privateKey, "utf-8")
+    console.log("KeyGenerated")
+  }
+)
